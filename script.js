31db73cd9eede0bbec20cc3ef839a6f1764bdeb4
ArthurR731 <arthurrocha3832@gmail.com>
2026-09-24 08:15:43 -0300

    bola e vy atualizado

--- script.js
+++ script.js
@@ -6,29 +6,36 @@ const ctx = canvas.getContext("2d");
 // vx - Define velocidade horizontal
 
 //personagem e onde estara/dimensoes:
-const player = {x: 40, y: 160, w: 32, h: 32, vx: 120}
+const player = {x: 40, y: 160, w: 32, h: 32, vx: 120, vy: 120}
 
-//Marca posição do quadro anterior:
+//Guarda do tempo do frame anterior:
 let last = 0; 
 
-//funcao que atualiza frames
+//funcao que atualiza frames, a posição do jogador
 function update(dt){
     player.x += player.vx * dt; 
     //Bater na parede esquerda ou direita = Inverter o sinal do vx
+    player.y += player.vy * dt; //Bater na parede cima ou debaixo = Inverter o sinal do vy
     if (player.x < 0 || player.x + player.w > canvas.width){ // || = ou
-        player.vx *= -1; // * -1 = inverter sinal
-    }
+        player.vx *= -1;
+        } // * -1 = inverter sinal
+        if (player.y < 0 || player.y + player.h > canvas.height){
+        player.vy *= -1;
+        }
 }
 
-//Função desenha o personagem
+//Função desenha o personagem, recriando ele a cada movimento 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height); //zera valores quando é redesenhado
     ctx.fillStyle = "#4ade80"; //cor personagem
-    ctx.fillRect(player.x, player.y, player.h, player.w); //passa as formas
-
+    //ctx.fillRect(player.x, player.y, player.h, player.w); //passa as formas definidas antes RETANGULO
     
     // ctx.fillStyle = "#fff";
     //ctx.fillRect(player.x, player.y, player.h, player.w);  NA COR BRANCA
+
+    ctx.beginPath(); //começa uma nova forma
+    ctx.arc(player.x + player.w /2, player.y + player.h /2, player.w /2, 0, Math.PI * 2); // ctx.arc(x, y, raio, inicio, fim) ele faz o circulo
+    ctx.fill(); //preencher
 
     ctx.fillText("O DeltaTime - dt independe da taxa de quadros", 12, 20); //texto dentro do quadro
 
@@ -36,9 +43,8 @@ function draw(){
 
 //funcao de loop, taxa de atualizacao, ts= taxa segundos
 function loop(ts){
-    if(!last) last = ts;
-    
-    const dt = Math.min(0.05, (ts - last)/1000); //1ms = 1seg. mathmin = contas
+    if(!last) last = ts; // ! = validacao, negação logica
+    const dt = Math.min(0.05, (ts - last)/1000); //1000ms = 1seg. mathmin = pega o menor valor dos numeros, compara 0,05 com (ts - last) / 1000
     last = ts;
     update(dt);
     draw();
@@ -46,3 +52,6 @@ function loop(ts){
 }
 
 requestAnimationFrame(loop); // de fora executa o primeiro disparo do personagem
+
+// Eu uso dt porque ele faz o movimento depender do tempo,
+// deixando a velocidade independente da taxa de quadros
